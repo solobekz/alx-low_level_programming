78508e80fb87be890cbc56fce35200ff6874473c
solobekz <solobekz@gmail.com>
2022-12-09 04:38:53 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,7 +13,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 	{
 		printf("%i is positive\n", n);
@@ -26,6 +25,5 @@ int main(void)
 	{
 		printf("%i is negative\n", n);
 	}
-
 	return (0);
 }
