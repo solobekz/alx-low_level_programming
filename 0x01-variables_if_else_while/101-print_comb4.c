63ed42afcda0ec21dd2a126330ff6f03211a5c30
solobekz <solobekz@gmail.com>
2022-12-09 12:46:54 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - print 3digit mumber
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int thou;
+	int tens;
+	int unit;
+
+	for (thou = '0'; thou <= '7'; thou++)
+	{
+		for (tens = '1'; tens <= '8'; tens++)
+		{
+			for (unit = '2'; unit <= '9'; unit++)
+			{
+				if (!(thou >= tens && tens >= unit))
+				{
+					putchar(thou);
+					putchar(tens);
+					putchar(unit);
+					if (!((thou == '7' && tens == '8') && unit == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
