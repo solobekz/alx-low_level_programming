a00839742a7ac20669c54f2800a24a66d26a0b5e
solobekz <solobekz@gmail.com>
2022-12-09 07:05:29 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print all sing digit number of base 10 from 0
+ * return: Always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%i", i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
