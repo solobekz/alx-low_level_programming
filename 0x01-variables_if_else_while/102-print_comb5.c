f8ba950f90ed9bd66482a74dd085cf6c3b8f4af2
solobekz <solobekz@gmail.com>
2022-12-09 20:47:40 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - print two 2 digit num
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int tens;
+	int unit;
+	int t;
+	int u;
+
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for (unit = '0'; unit <= '9'; unit++)
+		{
+			for (t = tens; t <= '9'; t++)
+			{
+				for (u = unit + 1; u <= '9'; u++)
+				{
+					putchar(tens);
+					putchar(unit);
+					putchar(' ');
+					putchar(t);
+					putchar(u);
+					if (!(tens == '9' && unit == '8' && t == '9' && u == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -14,3 +14,4 @@ Print all the letters except q and e
 9. Write a program that prints all possible combinations of single-digit numbers.
 10. Write a program that prints all possible different combinations of two digits.
 11. Write a program that prints all possible different combinations of three digits.
+12. Write a program that prints all possible combinations of two two-digit numbers.
