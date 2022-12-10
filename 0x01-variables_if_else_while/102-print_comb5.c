b07180217c25170601ff40c8b4b1a951fe083fe7
solobekz <solobekz@gmail.com>
2022-12-09 21:47:47 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,20 +18,22 @@ int main(void)
 		{
 			for (t = '0'; t <= '9'; t++)
 			{
-				for (u = unit + 1; u <= '9'; u++)
+				for (u = '0'; u <= '9'; u++)
 				{
-					putchar(tens);
-					putchar(unit);
-					putchar(' ');
-					putchar(t);
-					putchar(u);
-					if (!(tens == '9' && unit == '8' && t == '9' && u == '9'))
+					if (!(tens == '0' && unit == '0' && t == '0' && u == '0'))
 					{
-						putchar(',');
+						putchar(tens);
+						putchar(unit);
 						putchar(' ');
+						putchar(t);
+						putchar(u);
+						if (!(tens == '9' && unit == '8' && t == '9' && u == '9'))
+						{
+							putchar(',');
+							putchar(' ');
+						}
 					}
 				}
-				u = '0';
 			}
 		}
 	}
