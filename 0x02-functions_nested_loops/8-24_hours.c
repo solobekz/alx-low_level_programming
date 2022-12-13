cb83f6abd536c2d344d7fb63068d24830b1648af
solobekz <solobekz@gmail.com>
2022-12-13 13:24:29 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,7 +9,6 @@ void jack_bauer(void)
 {
 	int hour = 0;
 	int min = 0;
-	
 	while (hour < 24)
 	{
 		while (min < 60)
@@ -20,6 +19,8 @@ void jack_bauer(void)
 			_putchar('0' + (min / 10));
 			_putchar('0' + (min % 10));
 			_putchar('\n');
+			min++;
 		}
+		hour++;
 	}
 }
