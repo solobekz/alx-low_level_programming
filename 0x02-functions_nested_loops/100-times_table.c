35a0173b76dfef2fbbd2304345650b0e1ff3097b
solobekz <solobekz@gmail.com>
2022-12-14 00:24:53 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,9 +14,9 @@ void print_times_table(int n)
 	
 	if (!(n < 0 || n > 15))
 	{
-		for (row = 0; row <= 'n'; row++)
+		for (row = 0; row <= n; row++)
 		{
-			for (col = 0; col <= 'n'; col++)
+			for (col = 0; col <= n; col++)
 			{
 				pro = row * col;
 				if (col == 0)
