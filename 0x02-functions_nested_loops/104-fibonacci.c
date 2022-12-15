1a0281a8169a2c65f7e662ee10482e8b43c51dbd
solobekz <solobekz@gmail.com>
2022-12-15 01:18:24 -0800

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 98 fibonacci num
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long a = 1;
+	unsigned long b = 1;
+	unsigned long sum = 0;
+	long a_head, a_tail, b_head, b_tail, sum_head, sum_tail;
+	int c, overflow;
+
+	printf("1");
+
+	for (c = 2; counter <= 93; c++)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+		printf(", %lu", sum);
+	}
+
+	a_head = a / 1000000000; /* break larger num into 2 parts */
+	a_tail = a % 1000000000;
+	b_head = b / 1000000000;
+	b_tail = b % 1000000000;
+
+	for (; c < 93; c++)
+	{
+		overflow = (a_tail + b_tail) / 1000000000;
+		sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
+		sum_head = (a_head + b_head) + overflow;
+
+		printf(", %lu%lu", sum_head, sum_tail);
+		a_head = b_head;
+		a_tail = b_tail;
+		b_head = sum_head;
+		b_tail = sum_tail;
+	}
+	printf("\n");
+
+	return (0);
+}
