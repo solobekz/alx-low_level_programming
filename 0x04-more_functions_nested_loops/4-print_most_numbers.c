a1a215aacfbbe98fca93f17d3f04b309d656633d
solobekz <solobekz@gmail.com>
2022-12-17 13:20:00 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0-9 except 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = '0';
+	while (i <= '9')
+	{
+		if (i == '2' || i == '4')
+			break;
+		i++;
+		_putchar(i);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -41,5 +41,12 @@ int mul(int a, int b);
 
 void print_numbers(void);
 
+/**
+ * print_most_numbers - prints 0-9 except 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void);
+
 
 #endif
