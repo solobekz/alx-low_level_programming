2b473501452ac8c567d61979348d951dd102fafb
solobekz <solobekz@gmail.com>
2022-12-17 05:18:45 -0800

    isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
+
 int _putchar(char c);
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if true, 0 if false
+ */
 
 int _isupper(int c);
 
