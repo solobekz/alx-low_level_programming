9f3479b43ccdc79f410c8c70689e0b9b71d20efd
solobekz <solobekz@gmail.com>
2022-12-17 04:32:13 -0800

    convert_day in main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -12,9 +12,18 @@ void positive_or_negative(int i);
 /**
  * largest_number - check the largest number among 3 integers
  */
+
 int largest_number(int a, int b, int c);
 
+/**
+ * print_remaining_days - takes a date and prints how many days are left in the year, taking leap years into account
+ */
+
 void print_remaining_days(int month, int day, int year);
+
+/**
+ * convert_day - converts day of month to day of year, without accounting
+ */
 
 int convert_day(int month, int day);
 
