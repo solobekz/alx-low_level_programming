5e15f0f40e4ee47f8398f9a4c43417a0d30b51ad
solobekz <solobekz@gmail.com>
2022-12-17 11:09:14 -0800

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_remaining_days - takes a date and prints how many days are left in the year, taking leap years into account
+ * print_remaining_days - take a date and prints how many days are left in year
  * @month: month in number format
  * @day: day of month
  * @year: year
@@ -11,7 +11,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 && !(year % 100 == 0)) || (year % 400 == 0 && year % 100 == 0))
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,24 +5,38 @@
 
 /**
  * positive_or_negative - prints the sign of a number
+ * @i: char to check
  */
 
 void positive_or_negative(int i);
 
 /**
- * largest_number - check the largest number among 3 integers
+ * largest_number - check the largest number among
+ * 3 integers
+ * @a: integer 1
+ * @b: integer 2
+ * @c: integer 3
+ * Return: largest number
  */
 
 int largest_number(int a, int b, int c);
 
 /**
- * print_remaining_days - takes a date and prints how many days are left in the year, taking leap years into account
+ * print_remaining_days - takes a date and
+ * prints how many days are left in the year
+ * taking leap years into account
+ * @month: month
+ * @day: year in days
+ * @year: year
  */
 
 void print_remaining_days(int month, int day, int year);
 
 /**
- * convert_day - converts day of month to day of year, without accounting
+ * convert_day - converts day of month to dayof the year, without accounting
+ * @month: month
+ * @day: day of the month
+ * Return: day
  */
 
 int convert_day(int month, int day);
