9cfa27099d2bd62e11c9c73c8854ae77a83c2bd7
solobekz <solobekz@gmail.com>
2022-12-17 13:27:04 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,10 +12,10 @@ void print_most_numbers(void)
 	i = '0';
 	while (i <= '9')
 	{
-		if (i == '2' || i == '4')
-			break;
-
-		_putchar(i);
+		if (!(i == '2' || i == '4'))
+		{
+			_putchar(i);
+		}
 		i++;
 	}
 	_putchar('\n');
