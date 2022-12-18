41bc478be86c873ad0fe72e3ae4993f690265f8a
solobekz <solobekz@gmail.com>
2022-12-18 08:50:14 -0800

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints the largest prime factor of a number
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long n;
+	unsigned long d;
+
+	n = 612852475143;
+	d = 2;
+	while (d < n)
+	{
+		if (n % d == 0)
+		{
+			n /= d;
+			d = 2;
+		}
+		else
+		{
+			d++;
+		}
+	}
+	printf("%lu", n);
+
+	return (0);
+}
