97ee45681d821fcbdc2bc0bdc91aa1e82ea39692
solobekz <solobekz@gmail.com>
2022-12-18 04:54:24 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,13 @@
 void more_numbers(void)
 {
 	int i;
-	char j;
+	int j;
 
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j / 10 > '0')
+			if (j / 10 > 0)
 			{
 				_putchar(j / 10 + '0');
 			}
