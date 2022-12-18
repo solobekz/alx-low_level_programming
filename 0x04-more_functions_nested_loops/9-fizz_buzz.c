e6776b0a4d8d19424a4be75773166ad6fbf60cbe
solobekz <solobekz@gmail.com>
2022-12-18 07:41:20 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,17 +9,19 @@ int main(void)
 	int n;
 
 	n = 0;
-	while (n < 100)
+	while (n <= 100)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 
 		else if (n % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (n % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
-			printf("%d ", n);
+			printf("%d", n);
+		if (n < 100)
+			printf(" ");
 		n++;
 	}
 	printf("\n");
