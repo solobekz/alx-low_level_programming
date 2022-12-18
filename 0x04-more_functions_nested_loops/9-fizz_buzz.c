73a3d1d960b52b9fed55c6eb03b3784c1dd20cac
solobekz <solobekz@gmail.com>
2022-12-18 07:12:40 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1-100 with muliples of 3 as fizz and 5 as buzz
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n <= 100)
+	{
+		if (n % 3 == 0)
+			printf("Fizz ");
+
+		else if (n % 5 == 0)
+			printf("Buzz ");
+		else if ((n % 3 == 0) && (n % 5 == 0))
+			printf("FizzBuzz ");
+		else
+			printf("%d ", n);
+	}
+
+	return (0);
+}
