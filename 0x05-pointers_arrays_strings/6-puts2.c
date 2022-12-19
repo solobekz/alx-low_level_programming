01a582d7093b505304f669c191cc8b32b84cbc96
solobekz <solobekz@gmail.com>
2022-12-19 06:15:29 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+int _strlen(char *str)
+{
+	int len = 0;
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+
 /**
  * puts2 - prints strings in reverse
  * @str: strings to print
@@ -9,14 +20,8 @@
 void puts2(char *str)
 {
 	int i;
-	int len = 0;
 
-	while (*str != '\0')
-	{
-		len++;
-		str++;
-	}
-	for (i = 0; i < len; i += 2)
+	for (i = 0; i < _strlen(str); i += 2)
 	{
 		_putchar(str[i]);
 	}
