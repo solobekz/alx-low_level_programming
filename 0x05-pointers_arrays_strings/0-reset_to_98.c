f7cd16758645317b89fd3defadc1523ad3ec2f41
solobekz <solobekz@gmail.com>
2022-12-19 01:35:00 -0800

    readme, main.h and 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes a var value to 98
+ * @*n: pointer holding the var address
+ * Return: 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - prints a character
+ * @c: char to be printed
+ * Return: 0
+ */
+
+void _putchar(char c);
+
+/**
+ * reset_to_98 - changes a var value to 98
+ * @*n: a pinter to the address of the variable
+ * Return: void
+ */
+
+void reset_to_98(int *n);
+
+
+#endif
