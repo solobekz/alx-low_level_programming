5265061ae93ba6453deda7daa88353943701f55a
solobekz <solobekz@gmail.com>
2022-12-19 07:02:24 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -32,14 +32,14 @@ void puts_half(char *str)
 	j -= 1;
 	if (j % 2 == 0)
 	{
-		for (i = (j / 2); i <= j; i++)
+		for (i = (j / 2) + 1; i <= j; i++)
 		{
 			_putchar(str[i]);
 		}
 	}
 	else
 	{
-		for (i = ((j - 1)/2); i <= j; i++)
+		for (i = ((j - 1) / 2) + 1; i <= j; i++)
 		{
 			_putchar(str[i]);
 		}
