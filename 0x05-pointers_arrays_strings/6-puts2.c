2b6603ea380af5eac51268e212c8d8aae6d64faf
solobekz <solobekz@gmail.com>
2022-12-19 06:01:50 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -17,9 +17,9 @@ void puts2(char *str)
 		str++;
 	}
 	len--;
-	for (i = 0; i <= len; i=i+2)
+	for (i = 0; i <= len; i = i + 2)
 	{
-		_putchar(str[i]);
+		_putchar(*str[i]);
 	}
 	_putchar('\n');
 }
