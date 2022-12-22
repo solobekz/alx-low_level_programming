bd36350e160191f3146ddd9b302223fcf8f815b8
solobekz <solobekz@gmail.com>
2022-12-22 00:07:45 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,10 +14,13 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j = _strlen(dest);
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		dest[i + j] = src[i];
+		for (i = 0; i < n; i++)
+		{
+			dest[i + j] = src[i];
+		}
+		dest[i + j] = '\0';
 	}
-	dest[i + j] = '\0';
 	return (dest);
 }
