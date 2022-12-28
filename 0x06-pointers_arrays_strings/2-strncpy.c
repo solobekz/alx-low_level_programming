7bbff4c5d15171b3a0ffc97aa15996cc7b233b85
solobekz <solobekz@gmail.com>
2022-12-28 08:16:03 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,10 +14,15 @@ char *_strncpy(char *dest, char *src, int n)
 
 	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i < n && src[i] != '\0'; i++)
 		{
 			dest[i] = src[i];
 		}
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
 	}
 	return (dest);
 }
