7ce79bc803512cb6733935ffddf00fc221d8fb64
solobekz <solobekz@gmail.com>
2023-01-02 05:55:57 -0800

    _memcpy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,6 +11,16 @@
 
 char *_memset(char *s, char b, unsigned int n);
 
+/**
+ * _memcpy - copy memory fromsrc to dest
+ * @dest: destination
+ * @src: source
+ * @n: no of bytes to copy over
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
 
 
 #endif
