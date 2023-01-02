98c95b111440bc0f9962675a1b9c2251fc1f8acd
solobekz <solobekz@gmail.com>
2023-01-02 06:13:55 -0800

    strchr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -21,6 +21,15 @@ char *_memset(char *s, char b, unsigned int n);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
 
+/**
+ * _strchr - locates character froma string
+ * @s: string to locate from
+ * @c: char to locate
+ * Return: pointer to first occurence or null if absent
+ */
+
+char *_strchr(char *s, char c);
+
 
 
 #endif
