3771bd19128d9864ef2c06ae535fe99b03c3d489
solobekz <solobekz@gmail.com>
2023-01-04 11:18:04 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - prints char to standard output
+ * @c: char to print
+ * Return: ascii for the char
+ */
+
+int _putchar(char c);
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string
+ * Return: void
+ */
+
+void _puts_recursion(char *s);
+
+
+
+#endif
