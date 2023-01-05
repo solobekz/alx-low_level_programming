f7b491b9e61ebcb258502e0e9092d24563a48a0d
solobekz <solobekz@gmail.com>
2023-01-04 22:53:39 -0800

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,7 +13,7 @@ int check_palindrome(char *s, int b, int e)
 {
 	if (s[b] == s[e])
 	{
-		if (b == e)
+		if (b == e || b == e - 1)
 			return (1);
 		return (0 + check_palindrome(s, b + 1, e - 1));
 	}
