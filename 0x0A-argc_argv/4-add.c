b7e46b94323030a70d14b2ffeed4f9482975ef10
solobekz <solobekz@gmail.com>
2023-01-13 08:53:17 -0800

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - prints result of adding integers
+ * @argc: arg count
+ * @argv: arg array
+ * Return: 0 for success, 1 for error
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, res = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+	       for (j = 0; argv[i][j] != '\0'; j++)
+	       {
+		       if (!isdigit(argv[i][j]))
+		       {
+			       printf("Error\n");
+			       return (1);
+		       }
+	       }
+	       res += argv[i];
+	}
+	printf("%d\n", res);
+	return (0);
+}
