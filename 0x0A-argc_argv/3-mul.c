016393637da54ae03af0a59e90676325f12cc747
solobekz <solobekz@gmail.com>
2023-01-13 08:04:14 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,7 +5,7 @@
  * main - prints the result of multiplying 2 int
  * @argc: argument count
  * @argv: arguments array
- * Result: 1 is error, 0 if successful
+ * Return: 1 is error, 0 if successful
  */
 
 int main(int argc, char *argv[])
