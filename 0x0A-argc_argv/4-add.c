4e277a2061b4014de02cba04e5e503ca1ebe0ab8
solobekz <solobekz@gmail.com>
2023-01-13 09:23:35 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,15 +15,15 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-	       for (j = 0; argv[i][j] != '\0'; j++)
-	       {
-		       if (!isdigit(argv[i][j]))
-		       {
-			       printf("Error\n");
-			       return (1);
-		       }
-	       }
-	       res += atoi(argv[i]);
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		res += atoi(argv[i]);
 	}
 	printf("%d\n", res);
 	return (0);
