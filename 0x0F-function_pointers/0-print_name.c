aa63186132cdf9b613d0efb002497f30e55380eb
solobekz <solobekz@gmail.com>
2023-01-24 10:34:00 -0800

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,11 +5,10 @@
  * print_name - function to print name
  * @name: name to print
  * @f: function pointer that doesn't return anything
- * Return: Always 0 (Success)
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
+	if (name != NULL && f != NULL)
 		f(name);
 }
